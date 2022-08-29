ce6e316d2ec1c7ff69720c00dbc0122b609ccad6
Eleonora Esau <eleonora.esau@gmail.com>
2022-08-29 17:12:16 +0200

    edited user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :facilities
+  validates :first_name, :last_name, :password, :email, presence: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
