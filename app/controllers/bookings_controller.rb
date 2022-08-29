76e609c3f8ae1ff5c3c41186c28ab7adcfb67c32
Eleonora Esau <eleonora.esau@gmail.com>
2022-08-29 17:45:28 +0200

    added bookings controller

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,40 @@
+class BookingsController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[index show]
+  before_action :set_facility, only: %i[create]
+
+  def create
+    @booking = Booking.new(booking_params)
+    @facility = Facility.find(params[:facility_id])
+    @booking.facility = @facility
+    @booking.user = current_user
+    if @booking.save
+      redirect_to dashboard_path
+    else
+      render "facilities/show", status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    return unless @booking.user == current_user
+
+    @booking.destroy
+    redirect_to dashboard_path, status: :see_other
+  end
+
+  private
+
+  def set_facility
+    @facility = Facility.find(params[:facility_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:comment, :start_time, :end_time, :checked_in)
+  end
+end
