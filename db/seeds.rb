d7926142688da4be34d79721cb3348890169ce43
Eleonora Esau <eleonora.esau@gmail.com>
2022-08-30 14:29:45 +0200

    small seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Faker::Config.locale = 'de'
+Facility.destroy_all
+puts "Creating facilities..."
+20.times do |i|
+  facility = Facility.create!(
+    description: Faker::Quotes::Shakespeare.hamlet_quote,
+    address: Faker::Address.street_name,
+    category: %w[Fitness Climbing MMA Volleyball Swimming Badminton].sample,
+    capacity: rand(20..50)
+  )
+  puts "#{i + 1}. #{facility.capacity}"
+end
+puts "Finished!"
