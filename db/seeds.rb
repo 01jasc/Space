cd45005a067ffe855892142aa3126dbd09e29182
Fabian Schmidt <fabian@my-schmidt.de>
2022-08-30 16:10:23 +0200

    added bookings feature

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,11 +10,34 @@ Facility.destroy_all
 puts "Creating facilities..."
 20.times do |i|
   facility = Facility.create!(
+    name: Faker::TvShows::HeyArnold.location,
     description: Faker::Quotes::Shakespeare.hamlet_quote,
     address: Faker::Address.street_name,
     category: %w[Fitness Climbing MMA Volleyball Swimming Badminton].sample,
     capacity: rand(20..50)
   )
-  puts "#{i + 1}. #{facility.capacity}"
+  puts "#{i + 1}. #{facility.name}"
+end
+
+puts "Creating users..."
+1.times do |i|
+  user = User.create!(
+    email: "jan@test.com",
+    password: "123456",
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name
+  )
+end
+
+puts "Creating bookings..."
+9.times do |i|
+  booking = Booking.create!(
+    comment: Faker::TvShows::NewGirl.quote,
+    start_time: Faker::Date.in_date_period(year: 2022, month: 8),
+    end_time: Faker::Date.in_date_period(year: 2022, month: 9),
+    user_id: 1,
+    facility_id: 1,
+    checked_in: [true, false].sample
+  )
 end
 puts "Finished!"
