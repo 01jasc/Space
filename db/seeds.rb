3c9962ad1d1cf5669432d2ebcefc54d2169d2cc8
Fabian Schmidt <fabian@my-schmidt.de>
2022-09-01 16:50:49 +0200

    adding mma seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,7 +32,23 @@ Category.create!(
   capacity_type: "People",
   img: "climbing.png"
 )
-
+puts "creating MMA category"
+Category.create!(
+  title: "MMA",
+  explanation: "Mixed martial arts (MMA), sometimes referred to as cage fighting, no holds barred (NHB), and ultimate fighting, and originally referred to as Vale Tudo is a full-contact combat sport based on striking, grappling and ground fighting, incorporating techniques from various combat sports from around the world. The first documented use of the term mixed martial arts was in a review of UFC 1 by television critic Howard Rosenberg in 1993. The question of who actually coined the term is subject to debate.
+  During the early 20th century, various interstylistic contests took place throughout Japan and in the countries of the Four Asian Tigers.",
+  icon: "<i class='fa-solid fa-hand-fist'></i>",
+  capacity_type: "People",
+  img: "mma.png"
+)
+puts "creating volleyball category"
+Category.create!(
+  title: "Volleyball",
+  explanation: "Volleyball is a team sport in which two teams of six players are separated by a net. Each team tries to score points by grounding a ball on the other team's court under organized rules. It has been a part of the official program of the Summer Olympic Games since Tokyo 1964. Beach volleyball was introduced to the programme at the Atlanta 1996. The adapted version of volleyball at the Summer Paralympic Games is sitting volleyball.",
+  icon: "<i class='fa-solid fa-hand-fist'></i>",
+  capacity_type: "People",
+  img: "mma.png"
+)
 
 Faker::Config.locale = 'de'
 Facility.destroy_all
