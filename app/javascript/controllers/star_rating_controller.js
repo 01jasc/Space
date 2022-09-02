eb9960b2403094dc6b50be2a1210d6ce0162f203
Fabian Schmidt <fabian@my-schmidt.de>
2022-09-02 16:01:43 +0200

    controller

--- app/javascript/controllers/star_rating_controller.js
+++ app/javascript/controllers/star_rating_controller.js
@@ -1,6 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
+import StarRating from "star-rating.js"
 
-// Connects to data-controller="star-rating"
 export default class extends Controller {
   connect() {
     new StarRating(this.element)
