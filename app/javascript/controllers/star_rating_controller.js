fdc44419930854f59439c68447a89be9b8c481dd
Fabian Schmidt <fabian@my-schmidt.de>
2022-09-02 15:15:44 +0200

    added star form

--- app/javascript/controllers/star_rating_controller.js
+++ app/javascript/controllers/star_rating_controller.js
@@ -3,5 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="star-rating"
 export default class extends Controller {
   connect() {
+    new StarRating(this.element)
   }
 }
